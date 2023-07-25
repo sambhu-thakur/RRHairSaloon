a1e292debbffdbe39350e782d8dd9c75650cf71c
sambhu-thakur <62950837+sambhu-thakur@users.noreply.github.com>
2023-07-25 16:33:38 +0530

    Update CardComponent.js

--- src/shared/CardComponent.js
+++ src/shared/CardComponent.js
@@ -6,13 +6,13 @@ function  CardComponent () {
            <div className="row row-cols-1 row-cols-md-3 g-4">
   <div className="col shadow-lg p-1 mb-1 bg-white rounded ">
     <div className="card ">
-      <img src="assets/images/highlight.jpg" class="card-img-top" alt="..." height={340}/>
+      // <img src="assets/images/highlight.jpg" class="card-img-top" alt="..." height={340}/>
    
     </div>
   </div>
   <div className="col shadow-lg p-1 mb-1 bg-white rounded  ">
     <div className="card ">
-      <img src="assets/images/style1.jpg" class="card-img-top" alt="..." height={350}/>
+      // <img src="assets/images/style1.jpg" class="card-img-top" alt="..." height={350}/>
    
     </div>
   </div>
@@ -24,19 +24,19 @@ function  CardComponent () {
   </div>
   <div className="col shadow-lg p-1 mb-1 bg-white rounded  ">
     <div className="card ">
-      <img src="assets/images/Fade-Hairstyles.jpg" class="card-img-top" alt="..." height={350}/>
+      // <img src="assets/images/Fade-Hairstyles.jpg" class="card-img-top" alt="..." height={350}/>
    
     </div>
   </div>
   <div className="col  shadow-lg p-1 mb-1 bg-white rounded  ">
     <div className="card ">
-      <img src="assets/images/color.jpg" class="card-img-top" alt="..." height={350}/>
+      // <img src="assets/images/color.jpg" class="card-img-top" alt="..." height={350}/>
    
     </div>
   </div>
   <div className="col  shadow-lg p-1 mb-1 bg-white rounded  ">
     <div className="card h-100">
-      <img src="assets/images/style1.jpg" class="card-img-top" alt="..." height={350}/>
+      // <img src="assets/images/style1.jpg" class="card-img-top" alt="..." height={350}/>
    
     </div>
   </div>
