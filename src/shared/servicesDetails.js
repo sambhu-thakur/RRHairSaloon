9ebc369cc204a9ed81d4c42ee0b0ede3336843c7
sambhu-thakur <62950837+sambhu-thakur@users.noreply.github.com>
2024-01-28 16:15:11 +0530

    Update servicesDetails.js

--- src/shared/servicesDetails.js
+++ src/shared/servicesDetails.js
@@ -2,44 +2,44 @@ export const ServiceDetails =[
     {
         id:0,
         name: 'Men Haircut',
-        images : 'assets/images/Hairstyle1.jpg'
+        images : 'public/assets/images/Hairstyle1.jpg'
         
     },
     {
         id:1,
         name: 'Women Haircut',
-        images : 'assets/images/womenhaircut.jpg'
+        images : 'public/assets/images/womenhaircut.jpg'
         
     },
     {
         id:2,
         name: 'Kids Haircut',
-        images : 'assets/images/KidHaircut.jpg'
+        images : 'public/assets/images/KidHaircut.jpg'
         
     },
     {
         id:3,
         name: 'Tatoo',
-        images : 'assets/images/Haircut.jpg'
+        images : 'public/assets/images/Haircut.jpg'
         
     },
     {
         id:4,
         name: 'Nail styling',
-        images : 'assets/images/Nail.jpg'
+        images : 'public/assets/images/Nail.jpg'
         
     },
     {
         id:5,
         name: 'Makeup',
-        images : 'assets/images/Makeup.jpg'
+        images : 'public/assets/images/Makeup.jpg'
         
     },
 
     {
         id:6,
         name: 'Massage',
-        images : 'assets/images/Massage.jpg'
+        images : 'public/assets/images/Massage.jpg'
         
     },
    
@@ -47,14 +47,14 @@ export const ServiceDetails =[
     {
         id:7,
         name: 'Mehndi',
-        images : 'assets/images/Mehndi.jpg'
+        images : 'public/assets/images/Mehndi.jpg'
         
     }
     ,
     {
         id:8,
         name: 'color',
-        images : 'assets/images/Highlight.jpg'
+        images : 'public/assets/images/Highlight.jpg'
         
     }
 
